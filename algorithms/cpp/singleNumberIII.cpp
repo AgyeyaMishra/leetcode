9eb8dcfac1668d084332b87d4b67528aca688ed7
Agyeya Mishra <53916781+AgyeyaMishra@users.noreply.github.com>
2020-08-16 22:02:56 +0530

    Create singleNumberIII.cpp

---
+++ algorithms/cpp/singleNumberIII.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int xy = 0;
+        for(int n: nums) xy ^= n;
+        xy &= -xy;
+        vector<int> result = {0, 0};
+        for(int n: nums){
+            if(xy & n) result[0] ^= n;
+            else result[1] ^= n;
+        }
+        return result;
+    }
+};
