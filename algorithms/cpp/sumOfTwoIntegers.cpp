d2a2e874a46d581ee67d247c8f36e6a568c9a946
Agyeya Mishra <53916781+AgyeyaMishra@users.noreply.github.com>
2020-08-22 00:33:22 +0530

    Create sumOfTwoIntegers.cpp

---
+++ algorithms/cpp/sumOfTwoIntegers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int getSum(int a, int b) 
+    {
+     if (b > 0) 
+     { 
+        while (b > 0) 
+        { 
+            a++; 
+            b--; 
+        } 
+     } 
+     if (b < 0) // when 'b' is negative 
+     {  
+        while (b < 0) 
+        { 
+            a--; 
+            b++; 
+        } 
+     }
+        return a;
+   }
+};
