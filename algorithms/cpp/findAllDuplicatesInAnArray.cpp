432c4b31c849ef31fba106b9648cf62e9791307c
Agyeya Mishra <53916781+AgyeyaMishra@users.noreply.github.com>
2020-10-27 22:23:41 +0530

    Create findAllDuplicatesInAnArray.cpp

---
+++ algorithms/cpp/findAllDuplicatesInAnArray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> result;
+        for(int n : nums){
+            n = abs(n);
+            if(nums[n-1] > 0) nums[n-1] *= -1;
+            else result.push_back(n);
+        }
+        return result;
+    }
+};
